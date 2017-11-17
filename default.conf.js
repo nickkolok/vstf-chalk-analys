406b10d4c4f761fbf75a784d7ca881732a5c12cc
nickkolok <nickkolok@mail.ru>
2017-11-17 23:58:00 +0300

    Таки комментарии в конфиге

--- default.conf.js
+++ default.conf.js
@@ -1,23 +1,30 @@
 'use strict';
 
 var brights = [];
-for(var b = 256; b >=0; b-=1){
+
+// Пороги яркости ПО УБЫВАНИЮ
+// В принципе, нет разницы, как формировать массив
+// В данном случае числа слева направо:
+// максимальный порог яркости, минимальный порог яркости, шаг порога яркости
+for(var b = 120; b >=8; b-=4){
 	brights.push(b);
 }
 
 module.exports = {
-	filename: "./images/test3-1.png",
+	filename: "./images/test3-1.png", // На фиг не нужно
 	//filename: "./images/ex1-up1.png";
-	step: 1,
-	brightnessMin: 120,
-	countNormals: true,
-	delta: 50,
-	gaussRadius: 8,
-	centersCacheEnabled: true,
-	brights:brights,
-	writeSeparator: "\r\n",
-	readSeparator: /\r*\n/g,
-	smoothDelta: 2,
-	inpeakAvgCoeff:10,
-	scaleFactor:1,
+	step: 1, // Шаг прохода иголки. Можно потрогать, если скучно
+	brightnessMin: 120, // Дополнительный порог яркости. Нафиг
+	countNormals: true, // И это не трогать
+	delta: 50, // Плечо линейной регрессии для нормалей. Не трогать.
+	gaussRadius: 8, // Радиус размытия (не факт, что Гауссова). Не трогать.
+	centersCacheEnabled: true, // Не трогать!!!
+	brights:brights, // Магия
+	writeSeparator: "\r\n", // Если не нравится, как выводит
+	readSeparator: /\r*\n/g, // То же для ввода
+
+	// С этим можно и нужно играться
+	smoothDelta: 2, // Плечо линейно-регрессионного сглаживания перед расчётом расстояния между локальными максимумами. Имеет смысд от 1 до 10. Видно на фиолетовых и жёлтых точках
+	inpeakAvgCoeff:8, // Коэффициент усреднения яркости вдоль нормали. Имеет смысл от 1 до сколько фантазии хватит.
+	scaleFactor:1, // Коэффициент растяжения. До 8 имеет смысл. Лучше степень двойки.
 }
