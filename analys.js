0fe3d00f789a99973f12fa82dd2cdf80fdb41cd8
NickKolok <nickkolok@mail.ru>
2017-12-02 16:22:59 +0300

    Плоская вершинка теперь тоже локальный максимум

--- analys.js
+++ analys.js
@@ -465,11 +465,24 @@ function setTimeoutStubborn(fun, time) {
 	}, time);
 }
 
+function isLocMax(arr, i){
+	if(arr[i] > arr[i-1]){
+		var j = i+1;
+		while(arr[i] === arr[j]){
+			j++;
+		}
+		if(arr[i] > arr[j]) {
+			return true;
+		}
+	}
+	return false;
+}
+
 function getLocMaxs(arr){
 	var locmaxs = [];
 
 	for(var i = 1; i < arr.length - 1; i++){
-		if(arr[i] > arr[i+1] && arr[i] > arr[i-1]) {
+		if(isLocMax(arr,i)){
 			locmaxs.push(i);
 		}
 	}
